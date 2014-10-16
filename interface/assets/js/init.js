69998cb21fdaf385dc1f5fa81c78127dd8f59518
StephanHeijl <stephanheijl@gmail.com>
2014-10-17 00:16:50 +0200

    Added annotations to the side

--- interface/assets/js/init.js
+++ interface/assets/js/init.js
@@ -2,6 +2,8 @@ doc = undefined;
 elementHeight = undefined;
 bodyHeight = undefined;
 
+currentPage = 0;
+
 $(function() {
 
     $("link[rel=stylesheet-sass]").each(function() {
@@ -9,6 +11,7 @@ $(function() {
         var documentUrl = $(this).attr("href");
         $.get(documentUrl, function(scss) {
             css = Sass.compile(scss);
+			console.log(css);
             $("body").append("<style>"+css+"</style>");
         });
 
@@ -29,8 +32,6 @@ $(function() {
 			bodyHeight = parseInt(doc.find(".document-body").css("height"));
 			elementHeight = parseInt($(this).outerHeight(true));
 
-			console.log(bodyHeight, elementHeight, bodyHeight+elementHeight, 1100*0.6, $(this));
-
 			if(bodyHeight + elementHeight >= doc.innerHeight() - parseInt(doc.find(".document-content-padding").css("padding-top"))*2) {
 
 				children = $(this).children();
@@ -39,11 +40,10 @@ $(function() {
 					return;
 				}
 
-				console.log("Creating a new page.")
 				doc.appendTo($(".wrapper"));
 				doc = $(".document").eq(0).clone();
 				doc.find(".document-body").html("");
-				doc.appendTo($(".wrapper"));
+				doc.appendTo($(".document-wrapper"));
 
 			}
 			$(this).appendTo(doc.find(".document-body"));
@@ -53,9 +53,41 @@ $(function() {
 	setTimeout( function() {
 		doc = $(".document").eq(0).clone();
 		doc.find(".document-body").html("");
-		doc.appendTo($(".wrapper"));
+		doc.appendTo($(".document-wrapper"));
 		addToPages( $(".document-body").first().children() );
 		$(".document").eq(0).remove();
 	}, 1000)
+
+	/* Handle switching the annotation page */
+	$(window).on("scroll", function() {
+		var st = $("body").scrollTop();
+		tmpCurrentPage = -1;
+		$(".document").each(function() {
+			var distance = st - $(this).offset().top;
+			if( distance > 0 ) {
+				tmpCurrentPage++;
+			}
+		});
+
+		tmpCurrentPage = (tmpCurrentPage<0)?0:tmpCurrentPage;
+
+		if( tmpCurrentPage != currentPage) {
+			currentPage = tmpCurrentPage;
+			console.log("Switched page")
+			var annotationPages = $(".annotation-page");
+
+			if( annotationPages.length <= currentPage) {
+				return false;
+			}
+
+			console.log( " Annotations available. ")
+			var annotationPage = $(".annotation-page").eq(currentPage);
+			var paddingTop = parseInt($(".annotation-wrapper-padding").css("padding-top"));
+
+			$(".annotation-wrapper").animate({scrollTop: annotationPage.position().top - paddingTop}, 500);
+
+		}
+
+	});
 
 });
