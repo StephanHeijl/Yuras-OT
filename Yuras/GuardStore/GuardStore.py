0c8c4eb17d8ca5c00855b64b49258abe14e741e3
StephanHeijl <stephanheijl@gmail.com>
2014-11-30 00:53:34 +0100

    Removed all non-website related files

--- Yuras/GuardStore/GuardStore.py
+++
@@ -1,3 +0,0 @@
-class GuardStore(object):
-	def __init__(self):
-		pass
--- YurasSetup.sh
+++
@@ -1,8 +0,0 @@
-#sudo apt-get update
-sudo apt-get install python python-dev python-pip python-nose mongodb -y
-sudo mkdir /data
-sudo mkdir /data/db
-sudo chown -R vagrant /data 
-mongod > /dev/null &
-
-find -name "requirements.txt" | xargs -i sudo pip install -r {}
