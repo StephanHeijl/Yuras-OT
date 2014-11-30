5941a078aa0b556128f279173d355f7a889c20af
StephanHeijl <stephanheijl@gmail.com>
2014-12-01 00:30:59 +0100

    New navbar, Plans & Prices added, working navigation

---
+++ yuras.js
@@ -0,0 +1,15 @@
+$(".navbar a").click(function (e) {
+	e.preventDefault();
+
+	// Scroll to element with data-target.
+	var to = $(this).data("to"),
+		target = $("*[data-target=" + to + "]");
+
+	$("body").animate({
+		scrollTop: target.offset().top - $(".navbar").height()
+	})
+
+	$(this).parents("ul").find(".active").removeClass("active")
+	$(this).parent().addClass("active")
+
+});
