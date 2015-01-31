168c52570d9f5988b44e6bb013dccf76e18dc9fc
Stephan <stephanheijl@gmail.com>
2015-01-31 20:55:04 +0100

    Updated setup with more requirements not included in some linux systems

--- YurasSetup.sh
+++ YurasSetup.sh
@@ -2,7 +2,7 @@
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key 505A7412
 echo "deb [arch=amd64] http://s3.amazonaws.com/tokumx-debs $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/tokumx.list
 sudo apt-get update
-sudo apt-get install python python-dev python-pip python-nose haskell-platform texlive python-scipy python-numpy libssl-dev tokumx -y
+sudo apt-get install python python-dev python-pip python-nose haskell-platform texlive python-scipy python-numpy libssl-dev tokumx libffi-dev openssl libssl-dev -y
 # install pandoc
 sudo cabal update
 sudo cabal install pandoc
@@ -16,4 +16,3 @@ sudo chown -R vagrant /data
 mongod > /dev/null &
 
 find -name "requirements.txt" | xargs -i sudo pip install -r {}
-python -m textblob.download_corpora
