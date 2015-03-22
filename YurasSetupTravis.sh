c2cf580248a40727ad664e1183bf79bce8a01c0a
Stephan <stephan@yuras.nl>
2015-03-22 01:07:01 +0100

    Removed elasticsearch plugin from Travis installer

--- YurasSetupTravis.sh
+++ YurasSetupTravis.sh
@@ -22,13 +22,5 @@ echo ''{ \\\"_id\\\" : \\\"rs0\\\", \\\"members\\\" : [ {\\\"_id\\\" : 0, \\\"ho
 
 mongo localhost:27017 --eval "database='Yuras1'" setupDatabase.js
 
-# Install elasticsearch River plugin for MongoDB and Head Frontend
-sudo /usr/share/elasticsearch/bin/plugin --install com.github.richardwilly98.elasticsearch/elasticsearch-river-mongodb/2.0.6
-
-service elasticsearch restart
-
-# Configure index on document contents
-curl -XPUT '127.0.0.1:9200/_river/mongodb/_meta' '{"type":"mongodb","mongodb":{"db":"Yuras1","collection":"documents"},"index":{"name":"document_contents","type":"document"}}'
-
 # Install Python requirements
 find -name "requirements.txt" | xargs -i sudo pip install -r {}
