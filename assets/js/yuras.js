0d2ab08e9078856772da580743868f7a38f24ecc
Stephan <stephan@yuras.nl>
2015-11-10 21:19:44 +0100

    Moved Javascript into a block that doesn't run if Ghost is active.

--- assets/js/yuras.js
+++ assets/js/yuras.js
@@ -1,25 +1,37 @@
-$("a[data-to]").click(function (e) {
-	e.preventDefault();
+var GhostUserAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.121 Safari/535.2";
 
-	// Scroll to element with data-target.
-	var to = $(this).data("to"),
-		target = $("*[data-target=" + to + "]");
+if( navigator.userAgent !== GhostUserAgent ) {
 
-	$("body,html").animate({
-		scrollTop: target.offset().top - $(".navbar").height()
-	})
+	$("a[data-to]").click(function (e) {
+		e.preventDefault();
 
-	$(this).parents("ul").find(".active").removeClass("active")
-	$(this).parent().addClass("active")
+		// Scroll to element with data-target.
+		var to = $(this).data("to"),
+			target = $("*[data-target=" + to + "]");
 
-});
+		$("body,html").animate({
+			scrollTop: target.offset().top - $(".navbar").height()
+		})
 
-$(".feature-screenshot").each(function() {
-        featureLink = $("<a>").attr({"href": $(this).attr("src").replace("screenshots_thumbs", "screenshots"),"data-lightbox":"features","data-title":$(this).next().text()});
-        featureLink.insertBefore($(this));
-        $(this).appendTo(featureLink);
-});
+		$(this).parents("ul").find(".active").removeClass("active")
+		$(this).parent().addClass("active")
 
-$(function() {
+	});
+
+	$(".feature-screenshot").each(function() {
+			featureLink = $("<a>").attr({"href": $(this).attr("src").replace("screenshots_thumbs", "screenshots"),"data-lightbox":"features","data-title":$(this).next().text()});
+			featureLink.insertBefore($(this));
+			$(this).appendTo(featureLink);
+	});
+
 	$(".preloaded").attr("style", "").removeClass("preloaded");
-});
+
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-61117358-2', 'auto');
+	  ga('send', 'pageview');
+	
+}
