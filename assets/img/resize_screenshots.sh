aa5479438c203e9c37a070b3daeb61c0dffec426
Stephan <stephan@yuras.nl>
2015-11-10 21:41:44 +0100

    Removed demopage, decreased thumbnail size by another 10%.

--- assets/img/resize_screenshots.sh
+++ assets/img/resize_screenshots.sh
@@ -2,7 +2,7 @@ export THUMBSDIR="screenshots_thumbs"
 for screenshot in screenshots/*; do
 	export filename=$(basename $screenshot);
 	export output=("$THUMBSDIR/$filename");
-	convert $screenshot -resize 600x355 $output;
+	convert $screenshot -resize 555x326 $output;
 	pngquant --force --skip-if-larger --speed 1 --verbose --nofs -o $output 32 $output;
 	
 done
